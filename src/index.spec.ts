62bbed02f065b356622ad08bf4abfc561b95ff4a
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2021-03-30 23:23:26 +0200

    Improve tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,6 @@
-/* eslint-disable jest/no-done-callback */
-import { get, IncomingMessage, ServerResponse } from 'http';
+import { IncomingMessage, ServerResponse } from 'http';
 
+import axios from 'axios';
 import Debug from 'debug';
 
 import { WebServer } from './index';
@@ -16,194 +16,159 @@ class FakeSystemError extends Error {
 }
 
 describe('web-server tests', () => {
+
+    let webServer: WebServer | undefined;
+
+    afterEach(() => {
+        webServer?.close().catch(() => {/* Empty */});
+        jest.restoreAllMocks();
+    });
+
     it('can activate a simple web server', async () => {
         expect.assertions(1);
-        const webServer = new WebServer(() => {/* Empty */});
-        webServer.server.on('listening', () => webServer.close().then());
+        webServer = new WebServer(() => {/* Empty */});
+        webServer.server.on('listening', () => webServer?.close().then());
         await expect(webServer.listen()).resolves.toBeUndefined();
     });
 
-    it('can receive and respond to a request', async (done) => {
-        expect.assertions(2);
+    it('can receive and respond to a request', async () => {
+        expect.assertions(3);
 
-        const webServer = new WebServer((request: IncomingMessage, response: ServerResponse) => {
+        const data = 'Hello!';
+
+        webServer = new WebServer((request: IncomingMessage, response: ServerResponse) => {
             expect(request).toBeTruthy();
+            response.write(data);
             response.end();
         }, 3000);
 
-        webServer.server.on('listening', () => {
-            get('http://localhost:3000', async () => {
-                await webServer.close();
-                done();
-            });
-        });
+        await webServer.listen();
 
-        await expect(webServer.listen()).resolves.toBeUndefined();
+        const webResponse = await axios.get('http://localhost:3000');
+        expect(webResponse.status).toStrictEqual(200);
+        expect(webResponse.data).toStrictEqual(data);
     });
 
-    it('uses 8080 by default', (done) => {
+    it('uses 8080 by default', async () => {
         expect.assertions(3);
 
-        const webServer = new WebServer(() => {/* Empty */});
+        webServer = new WebServer(() => {/* Empty */});
         const debugSpy = jest.spyOn<any, any>(webServer, 'log');
-        webServer.listen().then(async () => {
-            expect(debugSpy).toHaveBeenNthCalledWith(1, `Creating listener on port 8080.`);
-            expect(debugSpy).toHaveBeenNthCalledWith(2, `Listening on port 8080.`);
-            expect(debugSpy).toHaveBeenCalledTimes(3);
-
-            debugSpy.mockRestore();
-            await webServer.close();
-            done();
-        });
+        await webServer.listen();
+        expect(debugSpy).toHaveBeenNthCalledWith(1, `Starting listener on port 8080.`);
+        expect(debugSpy).toHaveBeenNthCalledWith(2, `Listening on port 8080.`);
+        expect(debugSpy).toHaveBeenCalledTimes(3);
     });
 
-    it('emits announcement on listening', (done) => {
+    it('emits announcement on listening', async () => {
         expect.assertions(1);
 
-        const webServer = new WebServer(() => {/* Empty */});
+        webServer = new WebServer(() => {/* Empty */});
         const listeningSpy = jest.spyOn<WebServer, any>(webServer, 'announceListening');
-        webServer.listen().then(async () => {
-            expect(listeningSpy).toHaveBeenCalledWith();
-
-            listeningSpy.mockRestore();
-            await webServer.close();
-            done();
-        });
+        await webServer.listen();
+        expect(listeningSpy).toHaveBeenCalledWith();
     });
 
-    it('announces port on listening', (done) => {
+    it('announces port on listening', async () => {
         expect.assertions(3);
 
         const port = 5555;
-        const webServer = new WebServer(() => {/* Empty */}, port);
+        webServer = new WebServer(() => {/* Empty */}, port);
 
         const debugSpy = jest.spyOn<any, any>(webServer, 'log');
-        webServer.listen().then(async () => {
-            expect(debugSpy).toHaveBeenNthCalledWith(2, `Listening on port ${port}.`);
-            expect(debugSpy).toHaveBeenNthCalledWith(3, 'Ready for connections...');
-            expect(debugSpy).toHaveBeenCalledTimes(3);
-
-            debugSpy.mockRestore();
-            await webServer.close();
-            done();
-        });
+        await webServer.listen();
+        expect(debugSpy).toHaveBeenNthCalledWith(2, `Listening on port ${port}.`);
+        expect(debugSpy).toHaveBeenNthCalledWith(3, 'Ready for connections...');
+        expect(debugSpy).toHaveBeenCalledTimes(3);
     });
 
-    it('can log to debug', (done) => {
+    it('can log to debug', async () => {
         expect.assertions(2);
 
         const debug = Debug('test');
-        const webServer = new WebServer(() => {/* Empty */}, undefined, debug);
+        webServer = new WebServer(() => {/* Empty */}, undefined, debug);
 
         const debugSpy = jest.spyOn<any, any>(webServer, 'debug');
-        // eslint-disable-next-line sonarjs/no-identical-functions
-        webServer.listen().then(async () => {
-            expect(debugSpy).toHaveBeenNthCalledWith(3, 'Ready for connections...');
-            expect(debugSpy).toHaveBeenCalledTimes(3);
-
-            debugSpy.mockRestore();
-            await webServer.close();
-            done();
-        });
+        await webServer.listen();
+        expect(debugSpy).toHaveBeenNthCalledWith(3, 'Ready for connections...');
+        expect(debugSpy).toHaveBeenCalledTimes(3);
     });
 
     it('can listen and close async', async () => {
         expect.assertions(2);
 
-        const webServer = new WebServer(() => {/* Empty */});
+        webServer = new WebServer(() => {/* Empty */});
 
         await expect(webServer.listen()).resolves.toBeUndefined();
         await expect(webServer.close()).resolves.toBeUndefined();
     });
 
-    it('handles EADDRINUSE errors', (done) => {
+    it('handles EADDRINUSE errors', async () => {
         expect.assertions(3);
 
-        const webServer = new WebServer(() => {/* Empty */});
+        webServer = new WebServer(() => {/* Empty */});
         const debugSpy = jest.spyOn<any, any>(webServer, 'log');
         const systemError = new FakeSystemError('Port in use', 'listen', 'EADDRINUSE');
 
-        webServer.listen().then(async () => {
-            expect(() => {
-                webServer.server.emit('error', systemError);
-            }).toThrow('Port in use');
-            expect(debugSpy).toHaveBeenNthCalledWith(4, 'Port 8080 is already in use.');
-            expect(debugSpy).toHaveBeenCalledTimes(4);
-
-            debugSpy.mockRestore();
-            await webServer.close();
-            done();
-        });
+        await webServer.listen();
+        expect(() => {
+            webServer?.server.emit('error', systemError);
+        }).toThrow('Port in use');
+        expect(debugSpy).toHaveBeenNthCalledWith(4, 'Port 8080 is already in use.');
+        expect(debugSpy).toHaveBeenCalledTimes(4);
     });
 
-    it('handles EACCES errors', (done) => {
+    it('handles EACCES errors', async () => {
         expect.assertions(3);
 
-        const webServer = new WebServer(() => {/* Empty */});
+        webServer = new WebServer(() => {/* Empty */});
         const debugSpy = jest.spyOn<any, any>(webServer, 'log');
         const systemError = new FakeSystemError('Port need admin', 'listen', 'EACCES');
 
-        webServer.listen().then(async () => {
-            expect(() => {
-                webServer.server.emit('error', systemError);
-            }).toThrow('Port need admin');
-            expect(debugSpy).toHaveBeenNthCalledWith(4, 'Port 8080 requires elevated privileges.');
-            expect(debugSpy).toHaveBeenCalledTimes(4);
-
-            debugSpy.mockRestore();
-            await webServer.close();
-            done();
-        });
+        await webServer.listen();
+        expect(() => {
+            webServer?.server.emit('error', systemError);
+        }).toThrow('Port need admin');
+        expect(debugSpy).toHaveBeenNthCalledWith(4, 'Port 8080 requires elevated privileges.');
+        expect(debugSpy).toHaveBeenCalledTimes(4);
     });
 
-    it('handles other system errors', (done) => {
+    it('handles other system errors', async () => {
         expect.assertions(2);
 
-        const webServer = new WebServer(() => {/* Empty */});
+        webServer = new WebServer(() => {/* Empty */});
         const debugSpy = jest.spyOn<any, any>(webServer, 'log');
         const fakeError = new FakeSystemError('Exists', 'listen', 'EEXIST');
 
-        webServer.listen().then(async () => {
-            expect(() => {
-                webServer.server.emit('error', fakeError);
-            }).toThrow('Exists');
-            expect(debugSpy).toHaveBeenCalledTimes(3);
-
-            debugSpy.mockRestore();
-            await webServer.close();
-            done();
-        });
+        await webServer.listen();
+        expect(() => {
+            webServer?.server.emit('error', fakeError);
+        }).toThrow('Exists');
+        expect(debugSpy).toHaveBeenCalledTimes(3);
     });
 
-    it('handles other errors', (done) => {
+    it('handles other errors', async () => {
         expect.assertions(2);
 
-        const webServer = new WebServer(() => {/* Empty */});
+        webServer = new WebServer(() => {/* Empty */});
         const debugSpy = jest.spyOn<any, any>(webServer, 'log');
         const fakeError = new Error('Unknown error');
 
-        webServer.listen().then(async () => {
-            expect(() => {
-                webServer.server.emit('error', fakeError);
-            }).toThrow('Unknown error');
-            expect(debugSpy).toHaveBeenCalledTimes(3);
-
-            debugSpy.mockRestore();
-            await webServer.close();
-            done();
-        });
+        await webServer.listen();
+        expect(() => {
+            webServer?.server.emit('error', fakeError);
+        }).toThrow('Unknown error');
+        expect(debugSpy).toHaveBeenCalledTimes(3);
     });
 
-    it('rejects closing the server twice', (done) => {
+    it('rejects closing the server twice', async () => {
         expect.assertions(2);
 
-        const webServer = new WebServer(() => {/* Empty */});
+        webServer = new WebServer(() => {/* Empty */});
 
-        webServer.listen().then(async () => {
-            await expect(webServer.close()).resolves.toBeUndefined();
-            await expect(webServer.close()).rejects.toThrow('Server is not running.');
-            done();
-        });
+        await webServer.listen();
+        await expect(webServer?.close()).resolves.toBeUndefined();
+        await expect(webServer?.close()).rejects.toThrow('Server is not running.');
     });
 
 });
